08d3c406f93e813bf6d5c67953b7bc4435fbd323
Ryan Juang <ryan@cogito.(none)>
2014-02-18 01:01:14 -0500

    Solution for p5

--- p5.js
+++ p5.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log(1*2*3*4*5*6*7*8*9*10);
+function lcm(a,b) {
+  return (a * b) / (gcd(a,b));
+}
+
+function gcd(a,b) {
+
+}
+
+function brute() {
+  
+  function clean(m) {
+    var divisors = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    for (var i = 0; i < divisors.length; i++) {
+	if(m % divisors[i])
+	   return false
+    }
+    return true;
+  }
+
+  var i = 11;
+
+  while (!clean(i)) i++;
+
+  return i;
+}
+
+console.log(brute());
