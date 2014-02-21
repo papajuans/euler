7fa9e969d6ae2b2638d3e8761f6b52200918e372
Ryan J <papajuans@users.noreply.github.com>
2014-02-21 12:28:28 -0500

    Update p5.js

--- p5.js
+++ p5.js
@@ -10,18 +10,21 @@ function gcd(a,b) {
 
 function brute() {
   
-  function clean(m) {
+  function isCleanlyDivisible(m) {
     var divisors = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     for (var i = 0; i < divisors.length; i++) {
-	if(m % divisors[i])
-	   return false
+      if(m % divisors[i])
+        return false
     }
+    
     return true;
   }
 
   var i = 11;
 
-  while (!clean(i)) i++;
+  while (!isCleanlyDivisible(i)) {
+    i++;
+  }
 
   return i;
 }
